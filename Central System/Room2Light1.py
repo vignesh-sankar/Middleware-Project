8a2c086435d062c23a0b08518f6f725e56e67f55
Ubuntu <ubuntu@ip-172-31-20-204.us-west-2.compute.internal>
2017-06-13 01:39:05 +0000

    Completed except Arduino codes

--- Central System/Room2Light1.py
+++ Central System/Room2Light1.py
@@ -1,9 +1,10 @@
-from pubnub import Pubnub
+
+m pubnub import Pubnub
 import json
 
 pubnub = Pubnub(publish_key = 'pub-c-52dfaf38-9202-447f-a5c4-4fffdd0fdeac', subscribe_key = 'sub-c-09f86e3c-2e1e-11e7-97de-0619f8945a4f')
 
-DeviceStatus = False
+DeviceStatus = 0
 DeviceName = {}
 SendStatus = {}
 
@@ -48,26 +49,30 @@ def Initialization():
 def DeviceControlCallback(message,channel):
     global DeviceStatus
     print message
+    global SendStatus
     if(message == "Status"):
-        SendStatus={"Actuators":{
+            SendStatus={"Actuators":{
                     room:{
-                        device:DeviceStatus
+                        device:{
+                            "Status":DeviceStatus,"Channel":IndividualChannel
+                        }
                     }
                 }
-                };
-        pubnub.publish(channel = RetrieveStatusChannel, message = SendStatus)
+            };
+            print(SendStatus)
+            pubnub.publish(channel = RetrieveStatusChannel, message = SendStatus)
     else:
         split = message.split(":");
         if(split[0]=="True"):
             if(split[1]=="app"):
-                DeviceStatus = True
+                DeviceStatus = 1
             elif(split[1]=="sen"):
-                DeviceStatus = True
+                DeviceStatus = 1
         elif(split[0]=="False"):
             if(split[1]=="app"):
-                DeviceStatus = False
+                DeviceStatus = 0
             elif(split[1]=="sen"):
-                DeviceStatus = False
+                DeviceStatus = 0
 
 def StatusReportCallback(message,channel):
     global SendStatus
@@ -82,5 +87,4 @@ def StatusReportCallback(message,channel):
 Initialization()
 pubnub.subscribe( IndividualChannel, callback = DeviceControlCallback)
 pubnub.subscribe( StatusReportChannel, callback = StatusReportCallback)
-
 
